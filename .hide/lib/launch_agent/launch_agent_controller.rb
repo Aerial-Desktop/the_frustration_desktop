255a82029b93fff17eda63a6a09f4578e8cf30b4
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-06 12:18:56 -0400

    Launch Agent, create and set for screensaver to desktop script;

---
+++ .hide/lib/launch_agent/launch_agent_controller.rb
@@ -0,0 +1,33 @@
+# require relative screensaver_engine.rb
+# require relative com.example.plist
+# require relative .tmp
+
+# write out path needed
+class LaunchAgentController
+end
+# !/bin/bash
+
+# write out path needed
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LA=$DIR/../launch_agent
+SE=$DIR/../../bin
+
+echo $DIR
+
+#create .tmp file
+mkdir $LA/.tmp
+cp $LA/com.example.hello.plist $LA/.tmp/com.example.hello.plist
+
+# create the string needed.
+pwd2="              <string>$SE/./screensaver_engine.sh</string>"
+echo $pwd2
+# insert path needed into correct file.
+# line 9 in com.example.hello.plist
+sed -i '' '9s?.*?'"$pwd2"'?' $LA/.tmp/com.example.hello.plist
+
+# change environment appropriately
+chmod 644 $LA/.tmp/com.example.hello.plist
+
+cp $LA/.tmp/com.example.hello.plist ~/Library/LaunchAgents/
+
+launchctl load ~/Library/LaunchAgents/com.example.hello.plist
