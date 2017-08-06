b937d22963ec7a47b4828b1229d2bb4a9e929e33
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-06 11:35:16 -0400

    Install Screensaver, if not in correct location open saver in app.

---
+++ .hide/lib/screensaver/screensaver_controller.rb
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# run aerial application
+  if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
+    echo "File exists."
+  else
+    open $DIR/../../bin/Aerial.saver/
+  fi
+# will have require_relative '../check/check_module'
+# class ScreensaverController
+#
+# end
